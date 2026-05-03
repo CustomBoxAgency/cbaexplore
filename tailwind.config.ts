582e2245fe26a57db13cbcd934b93f856e296ebd
CustomBoxAgency <mark@customboxagency.com>
2026-05-02 19:18:01 -0500

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand1: "var(--brand1)",
+        brand2: "var(--brand2)",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
